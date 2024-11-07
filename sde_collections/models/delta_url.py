8bb7fc4293541c7f3174344ce17585cafb9e799c
Carson Davis <CarsonReidDavis@gmail.com>
2024-11-07 11:43:33 -0600

    add helper functions to url models

--- sde_collections/models/delta_url.py
+++ sde_collections/models/delta_url.py
@@ -1,3 +1,6 @@
+import os
+from urllib.parse import urlparse
+
 from django.db import models
 
 from .collection_choice_fields import Divisions, DocumentTypes
@@ -17,6 +20,45 @@ class BaseUrl(models.Model):
     class Meta:
         abstract = True
         ordering = ["url"]
+
+    @property
+    def fileext(self) -> str:
+        # Parse the URL to get the path
+        parsed_url = urlparse(self.url)
+        path = parsed_url.path
+
+        # Check for cases where the path ends with a slash or is empty, implying a directory or default file
+        if path.endswith("/") or not path:
+            return "html"
+
+        # Extract the extension from the path
+        extension = os.path.splitext(path)[1]
+
+        # Default to .html if no extension is found
+        if not extension:
+            return "html"
+
+        if extension.startswith("."):
+            return extension[1:]
+        return extension
+
+    def splits(self) -> list[tuple[str, str]]:
+        """Split the path into multiple collections."""
+        parts = []
+        part_string = ""
+        for part in self.path.split("/"):
+            if part:
+                part_string += f"/{part}"
+                parts.append((part_string, part))
+        return parts
+
+    @property
+    def path(self) -> str:
+        parsed = urlparse(self.url)
+        path = f"{parsed.path}"
+        if parsed.query:
+            path += f"?{parsed.query}"
+        return path
 
     def __str__(self):
         return self.url
